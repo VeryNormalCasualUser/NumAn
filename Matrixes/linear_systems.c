3d7f1be9a98943b68831a9169c13ae883b6700bf
Yazan Al-Aswad <yazan.al-aswad@telavox.com>
2024-02-11 19:29:03 +0100

    implement jacobi method, must add refactoring

--- Matrixes/linear_systems.c
+++ Matrixes/linear_systems.c
@@ -6,7 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define ITER 50
 
+// determinant for Triangular and Diagonal matrixes
 double det_T(const unsigned int n, double T[n][n]) {
   double res = 0;
   for (unsigned int i = 0; i < n; ++i)
@@ -69,10 +71,10 @@ void gauss_elim(const unsigned int n, double A[n][n], double b[n]) {
 }
 
 void factor_LU(const unsigned int n, double A[n][n], double L[n][n]) {
-  //assumes L is a 0 matrix
+  // assumes L is a 0 matrix
   for (unsigned int i = 0; i < n - 1; ++i) {
     double a = A[i][i];
-    if (fabs(a) < P){
+    if (fabs(a) < P) {
       printf("pivot is zero\n");
       break;
     }
@@ -91,8 +93,8 @@ void factor_LU(const unsigned int n, double A[n][n], double L[n][n]) {
 
 double *solve_LU(const unsigned int n, double A[n][n], double b[n]) {
   double L[n][n];
-  for (unsigned int i = 0; i < n; i++){
-    for (unsigned int j = 0; j < n; j++){
+  for (unsigned int i = 0; i < n; i++) {
+    for (unsigned int j = 0; j < n; j++) {
       L[i][j] = 0;
     }
   }
@@ -113,7 +115,6 @@ double *solve_LU(const unsigned int n, double A[n][n], double b[n]) {
 
     free(y);
     return x;
-
   } else {
     return NULL;
   }
@@ -137,6 +138,89 @@ double *solve(const unsigned int n, double A[n][n], double b[n]) {
   } else {
     return NULL;
   }
+}
+
+void jacobi_iteration(const unsigned int n, double D_1_LpU[n][n], double xk[n],
+                      double xk_1[n], double D_1b[n]) {
+  double D_1_LpUxk[n];
+  print_array_wt(n, xk, "jacobi iteration for xk:");
+
+  for (unsigned int i = 0; i < n; ++i) {
+    D_1_LpUxk[i] = 0.0;
+    for (unsigned int j = i + 1; j < n; ++j) {
+      D_1_LpUxk[i] += (-D_1_LpU[i][j]) * xk[j];
+    }
+  }
+  for (unsigned int i = 1; i < n; ++i) {
+    for (unsigned int j = 0; j < i; ++j) {
+      D_1_LpUxk[i] += (-D_1_LpU[i][j]) * xk[j];
+    }
+  }
+
+  double left;
+  double right;
+  for (unsigned int i = 0; i < n; ++i) {
+    left = D_1_LpUxk[i];
+    right = D_1b[i];
+    printf("xk[%u] = %lf + %lf = %lf\n", i, left, right,
+           xk_1[i] = left + right);
+  }
+}
+
+double *jacobi_method(const unsigned int n, int iterations, double A[n][n],
+                      double xk[n], double b[n]) {
+  double D_1[n];
+  double D_1b[n];
+  double D_1_LpU[n][n];
+  print_matrix_wt(n, A, "A:");
+  print_array_wt(n, xk, "x0:");
+  print_array_wt(n, b, "b:");
+  for (unsigned int i = 0; i < n; ++i) {
+    D_1[i] = 1 / A[i][i];
+    A[i][i] = 0;
+  }
+
+  print_matrix_wt(n, A, "L+U");
+  print_array_wt(n, D_1, "D^1");
+
+  for (unsigned int i = 0; i < n; ++i) {
+    D_1b[i] = D_1[i] * b[i];
+  }
+
+  for (unsigned int i = 0; i < n; ++i) { // optimize
+    for (unsigned int j = 0; j < n; ++j) {
+      D_1_LpU[i][j] = D_1[i] * A[i][j];
+    }
+  }
+
+  print_matrix_wt(n, D_1_LpU, "D^-1 * (L+U):");
+
+  print_array_wt(n, D_1b, "D^-1 * b:");
+  double *xk_1 = calloc(n, sizeof(double));
+  double *temp;
+
+  const int it = iterations; // FOR DEBUG
+  while (iterations > 0) {
+    jacobi_iteration(n, D_1_LpU, xk, xk_1, D_1b);
+    printf("x%d:\n", it - iterations);
+    print_array(n, xk);
+    printf("x%d:\n", it - iterations + 1);
+    print_array(n, xk_1);
+    temp = xk;
+    xk = xk_1;
+    xk_1 = temp;
+    temp = NULL;
+    for (unsigned int i = 0; i < n; ++i) {
+      xk_1[i] = 0.0;
+    }
+    iterations--;
+  }
+
+  double *res = calloc(n, sizeof(double));
+  memcpy(res, xk, n * sizeof(double));
+  free(xk);
+  free(xk_1);
+  return res;
 }
 
 void test_back_sub(void) {
@@ -206,7 +290,7 @@ void test_solver(void) {
   A[1][1] = 1;
   b[1] = 1;
 
-  double* res = back_sub(n, A, b);
+  double *res = back_sub(n, A, b);
   printf("back_sub:\n");
   print_array(n, res);
   free(res);
@@ -256,7 +340,168 @@ void test_solver(void) {
   free(x);
 }
 
+void testJacobi2x2(void) {
+  unsigned int n = 2;
+  double A[n][n];
+  double b[n];
+
+  A[0][0] = 2;
+  A[0][1] = 1;
+  A[1][0] = 3;
+  A[1][1] = 4;
+
+  b[0] = 3;
+  b[1] = 5;
+
+  double *x0 = calloc(n, sizeof(double));
+  x0[0] = 0;
+  x0[1] = 1;
+
+  double *res = jacobi_method(n, ITER, A, x0, b);
+  assert(fabs(res[0] - 1.4) < P);
+  assert(fabs(res[1] - 0.2) < P);
+  free(res);
+}
+
+void testJacobi3x3(void) {
+  unsigned int n = 3;
+  double A[n][n];
+  double b[n];
+
+  A[0][0] = 3;
+  A[0][1] = 1;
+  A[0][2] = 1;
+  A[1][0] = 1;
+  A[1][1] = 3;
+  A[1][2] = 1;
+  A[2][0] = 1;
+  A[2][1] = 1;
+  A[2][2] = 3;
+
+  b[0] = 3;
+  b[1] = 5;
+  b[2] = 7;
+
+  double *x0 = calloc(n, sizeof(double));
+  x0[0] = 0.1;
+  x0[1] = 1.1;
+  x0[2] = 2.1;
+
+  double *res = jacobi_method(n, ITER, A, x0, b);
+  for (unsigned int i = 0; i < n; ++i) {
+    assert(fabs(res[i] - i) < P);
+  }
+  free(res);
+}
+
+void testJacobi4x4(void) {
+
+  unsigned int n = 4;
+  double A[n][n];
+  double b[n];
+
+  A[0][0] = 3;
+  A[0][1] = 1;
+  A[0][2] = 1;
+  A[0][3] = 0;
+
+  A[1][0] = 1;
+  A[1][1] = 6;
+  A[1][2] = 3;
+  A[1][3] = -1;
+
+  A[2][0] = 6;
+  A[2][1] = 0;
+  A[2][2] = 9;
+  A[2][3] = -2;
+
+  A[3][0] = 1;
+  A[3][1] = 0;
+  A[3][2] = -1;
+  A[3][3] = -7;
+
+  b[0] = 1;
+  b[1] = 1;
+  b[2] = 1;
+  b[3] = 1;
+
+
+  double *x0 = calloc(n, sizeof(double));
+  for (unsigned int i = 0; i < n; ++i) {
+    x0[i] = 1.1;
+  }
+
+  double *res = jacobi_method(n, ITER, A, x0, b);
+  assert(fabs(norm(n, res)) - 0.385746 < P*10);
+  free(res);
+}
+
+void testJacobi6x6(void) {
+
+  unsigned int n = 6;
+  double A[n][n];
+  double b[n];
+
+  A[0][0] = 3;
+  A[0][1] = -1;
+  A[0][2] = 0;
+  A[0][3] = 0;
+  A[0][4] = 0;
+  A[0][5] = 1 / 2;
+  A[1][0] = -1;
+  A[1][1] = 3;
+  A[1][2] = -1;
+  A[1][3] = 0;
+  A[1][4] = 1 / 2;
+  A[1][5] = 0;
+  A[2][0] = 0;
+  A[2][1] = -1;
+  A[2][2] = 3;
+  A[2][3] = -1;
+  A[2][4] = 0;
+  A[2][5] = 0;
+  A[3][0] = 0;
+  A[3][1] = 0;
+  A[3][2] = -1;
+  A[3][3] = 3;
+  A[3][4] = -1;
+  A[3][5] = 0;
+  A[4][0] = 0;
+  A[4][1] = 1 / 2;
+  A[4][2] = 0;
+  A[4][3] = -1;
+  A[4][4] = 3;
+  A[4][5] = -1;
+  A[5][0] = 1 / 2;
+  A[5][1] = 0;
+  A[5][2] = 0;
+  A[5][3] = 0;
+  A[5][4] = -1;
+  A[5][5] = 3;
+
+  b[0] = 5 / 2;
+  b[1] = 3 / 2;
+  b[2] = 1;
+  b[3] = 1;
+  b[4] = 3 / 2;
+  b[5] = 5 / 2;
+
+  double *x0 = calloc(n, sizeof(double));
+  for (unsigned int i = 0; i < n; ++i) {
+    x0[i] = 1.1;
+  }
+
+  double *res = jacobi_method(n, ITER, A, x0, b);
+  for (unsigned int i = 0; i < n; ++i) {
+    assert(fabs(res[i] - 1) < P);
+  }
+  free(res);
+}
+
 int main() {
   fesetround(FE_TONEAREST);
-  test_solver();
+  testJacobi2x2();
+  testJacobi3x3();  
+  testJacobi6x6();
+  testJacobi4x4();
 }
--- numan_utils.h
+++ numan_utils.h
@@ -1,3 +1,4 @@
+#include <math.h>
 #define P 1E-8
 #define POW 1000000
 #include <stdio.h>
@@ -11,11 +12,20 @@ double derivative(double (*f)(double), double value, double precision) {
   return (x1 - x0) / (2 * precision);
 }
 
-void print_array(unsigned int n, double b[n]) {
+void print_array_wt(unsigned int n, double b[n], char* str) {
+  printf("%s\n", str);
   for (unsigned int i = 0; i < n; ++i) {
     printf("%lf ", b[i]);
   }
-  printf("\n");
+  printf("\n\n");
+}
+
+void print_array(unsigned int n, double b[n]) {
+
+  for (unsigned int i = 0; i < n; ++i) {
+    printf("%lf ", b[i]);
+  }
+  printf("\n\n");
 }
 
 void print_matrix(unsigned int n, double A[n][n]) {
@@ -23,5 +33,23 @@ void print_matrix(unsigned int n, double A[n][n]) {
   for (unsigned int i = 0; i < n; ++i) {
     print_array(n, A[i]); 
   }
-  printf("\n");
+  printf("\n\n");
+}
+
+void print_matrix_wt(unsigned int n, double A[n][n], char* str) {
+  printf("%s\n", str);
+  for (unsigned int i = 0; i < n; ++i) {
+    print_array(n, A[i]);
+  }
+  printf("\n\n");
+}
+
+double norm(unsigned int n, double vector[n]){
+  double sum = 0;
+  double curr;
+  for (unsigned int i = 0; i < n; ++i){
+    curr = vector[i];
+    sum += curr*curr;
+  }
+  return sqrt(sum);
 }
